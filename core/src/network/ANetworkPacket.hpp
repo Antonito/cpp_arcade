18c233533b4bcc0a05bb3a5a182f530f61e88bdf
Antoine Baché <antoine.bache@epitech.eu>
2017-03-28 14:58:47 +0200

    working on network

---
+++ core/src/network/ANetworkPacket.hpp
@@ -0,0 +1,13 @@
+#ifndef ANETWORK_PACKET_HPP
+#define ANETWORK_PACKET_HPP
+
+#include "INetworkPacket.hpp"
+
+class ANetworkPacket : public INetworkPacket
+{
+public:
+protected:
+private:
+};
+
+#endif // !ANETWORK_PACKET_HPP
