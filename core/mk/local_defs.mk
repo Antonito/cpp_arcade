354537f357770a515f3956175554265762cb739c
Antoine Baché <antoine.bache@epitech.eu>
2017-04-09 03:41:31 +0200

    Added exceptions to all Makefiles

--- core/mk/local_defs.mk
+++ core/mk/local_defs.mk
@@ -3,6 +3,6 @@ NAME=			$(ROOT_DIR)/arcade
 
 LOCAL_COMP_FLAGS=
 
-LOCAL_LINK_FLAGS=	-ldl -L$(ROOT_DIR)/build/ -larcade_common -larcade_network -lgames_common
+LOCAL_LINK_FLAGS=	-ldl -L$(ROOT_DIR)/build/ -larcade_common -larcade_network -lgames_common -larcade_exceptions
 
 LOCAL_DEBUG_FLAGS=
--- games_sources/pong/mk/local_defs.mk
+++ games_sources/pong/mk/local_defs.mk
@@ -3,6 +3,6 @@ NAME=			$(ROOT_DIR)/games/libarcade_pong.so
 
 LOCAL_COMP_FLAGS=	-fPIC
 
-LOCAL_LINK_FLAGS=	-shared -rdynamic -L$(ROOT_DIR)/build -larcade_common -lgames_common
+LOCAL_LINK_FLAGS=	-shared -rdynamic -L$(ROOT_DIR)/build -larcade_common -lgames_common -larcade_exceptions
 
 LOCAL_DEBUG_FLAGS=
